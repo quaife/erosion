d3eb724d7c3e160ccf684a6f7feb64c8bc323974
Nick Moore <nickmoore83@gmail.com>
2016-12-04 15:44:08 -0500

    NM

--- julia/zdrive.jl
+++ julia/zdrive.jl
@@ -1,13 +1,10 @@
 # Driver
-function driver(geoinfile::AbstractString, tfin::Float64=1.)
+function driver(geoinfile::AbstractString, tfin::Float64=0.5)
 	include("basic.jl")
 	# Extract the geometry from a data file.
 	thlenvec,npts = readgeodata(geoinfile)
 	# Optional: set dt based on npts.
 	dt = 0.2/npts
-
-	tfin = 7*dt
-	
 	# Call the main erosion routine.
 	erosion(tfin,dt,thlenvec; axlims=[1.,1.])
 end
@@ -16,6 +13,7 @@ function run()
 	include("basic.jl")
 	geoinfile = "../datafiles/thlen.dat"
 	make4circs(geoinfile,64,4)
-	driver(geoinfile)
+	tfin = 0.03
+	driver(geoinfile,tfin)
 end
 run()
