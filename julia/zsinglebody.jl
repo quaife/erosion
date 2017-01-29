73febefe38007dec82b66c75b57fa24f4d298a43
Nick Moore <nickmoore83@gmail.com>
2017-01-29 16:59:42 -0500

    Added zsinglebody.jl to do erosion of a single body.

---
+++ julia/zsinglebody.jl
@@ -0,0 +1,21 @@
+# Driver
+function driver(thlenfile::AbstractString, tfin::Float64=0.5)
+	include("basic.jl")
+	# Extract the geometry from a data file.
+	thlenvec,npts = readthlenfile(thlenfile)
+	# Optional: set dt based on npts.
+	dt = 0.1/npts
+	# Call the main erosion routine.
+	erosion(tfin,dt,thlenvec;lenevo=0,axlims=[1.,1.])
+end
+
+function run()
+	include("basic.jl")
+	thlenfile = "../datafiles/thlen.dat"
+	npts = 256
+	rad = 0.2
+	make1circ(thlenfile,npts,rad)
+	tfin = 0.1
+	driver(thlenfile,tfin)
+end
+run()
