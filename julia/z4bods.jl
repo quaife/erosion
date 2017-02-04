ddc7b898474346c341981e05f4354a5290b05f28
Nick Moore <nickmoore83@gmail.com>
2017-02-04 09:17:41 -0500

    Changed names of driver files. Now z1bod and z4bods

---
+++ julia/z4bods.jl
@@ -0,0 +1,18 @@
+# Driver
+function run()
+	include("basic.jl")
+	# Parameters
+	thlenfile = "../datafiles/thlen.dat"
+	npts = 256
+	nbods = 4
+	dt = 0.1/npts
+	tfin = 0.03
+	# Make the geometry and write to a data file.
+	make4circs(thlenfile,npts,nbods)
+	# Extract the geometry from a data file.
+	thlenvec,npts = readthlenfile(thlenfile)
+	# Call the main erosion routine.
+	erosion(tfin,dt,thlenvec)
+end
+
+run()
