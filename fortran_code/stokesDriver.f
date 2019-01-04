2f06d290745d6eefe2f9201caa6507d41af1ddb8
Shang-Huan Chiu <shang-huan@wc-dhcp197d100.employee-secure.wireless.fsu.edu>
2019-01-04 15:30:39 -0500

    upload  Stokes Driver and Solver for Barycentric

--- fortran_code/stokesDriver.f
+++ fortran_code/stokesDriver.f
@@ -1,12 +1,15 @@
       program stokesDriver
       implicit real*8 (a-h,o-z)
 
-      parameter (ninner = 256)
+      parameter (ninner = 2**8)
       parameter (nbodies = 2)
       parameter (nouter = 2**8)
       parameter (ntargets = 120)
       parameter (maxbodies = 10)
 
+c      parameter (ninnc = 2**8,nbeta = 1,ninner = nbeta*ninnc)      
+c      parameter (noutc = 2**8, nouter = nbeta*noutc)      
+      
       dimension centerx(maxbodies),centery(maxbodies)
       dimension radius(maxbodies),phi(maxbodies)
       dimension x(ninner*nbodies),y(ninner*nbodies)
@@ -21,25 +24,48 @@
       dimension vtar(ntargets*ntargets)
       dimension press_tar(ntargets*ntargets)
       dimension vort_tar(ntargets*ntargets)
+      
+c     Testing for stress tensor       
+      dimension xtar_test(ninner*nbodies)
+      dimension ytar_test(ninner*nbodies)
+      dimension utar_test(ninner*nbodies)
+      dimension vtar_test(ninner*nbodies)
+      dimension press_tar_test(ninner*nbodies)
+      dimension vort_tar_test(ninner*nbodies)      
+      
+      
       dimension iside(ntargets*ntargets)
       dimension inear(ntargets*ntargets)
 
       pi = 4.d0*datan(1.d0)
       twopi = 2.d0*pi
       dtheta = twopi/dble(ninner)
+    
 
-      centery(1) = 0.0d0
-      centerx(2) = 0.5d0
-      centerx(3) = -0.4d0
-      centery(2) = -0.1d0
-      centery(3) = 0.1d0
+!      centery(1) = 0.0d0
+!      centerx(2) = 0.5d0
+!      centerx(3) = -0.4d0
+!      centery(2) = -0.1d0
+!      centery(3) = 0.1d0
+!      radius(1) = 2.d-1
+!      radius(2) = 1.d-1
+!      radius(3) = 5.d-2
+!      phi(1) = 0.d0
+!      phi(2) = 0.d0
+!      phi(3) = pi/4.d0
+
       radius(1) = 2.d-1
-      radius(2) = 1.d-1
-      radius(3) = 5.d-2
+      radius(2) = 2.d-1
+      centerx(1) = 0.5d0
+      centerx(2) = 0.5d0 - radius(1) - radius(2) - 0.001d0
+      centery(1) = 0.1d0 + 0.499d0
+      centery(2) = 0.1d0 + 0.499d0
+
+c      radius(3) = 5.d-2
       phi(1) = 0.d0
       phi(2) = 0.d0
-      phi(3) = pi/4.d0
-
+c      phi(3) = pi/4.d0
+      
 c      Nphi = 128
 c      dphi = twopi/Nphi
 c      open(unit=21,file='output/dragx.dat')
@@ -62,6 +88,43 @@ c        phi(1) = dble(kk-1)*dphi
       enddo
 
 
+c      do j = 1,nbodies
+c        do k = 1,ninner
+c          alpha = 90.d0*pi/180.d0
+c          beta = pi - alpha
+c          var_rad = radius(j)
+c          if(k .lt. ninner/2+1) then
+c          x((j-1)*ninner+k) = !-var_rad*dcos(pi/4.d0)+
+c     $          var_rad*(dcos(k*2.d0*alpha/ninner+beta/2.d0))
+c          y((j-1)*ninner+k) = -var_rad*dcos(alpha/2.d0)+
+c     $          var_rad*(dsin(k*2.d0*alpha/ninner+beta/2.d0))
+c          else
+c          x((j-1)*ninner+k) = !var_rad*dcos(pi/4.d0)+
+c     $       var_rad*(dcos(k*2.d0*alpha/ninner+3.d0*beta/2.d0))
+c          y((j-1)*ninner+k) = var_rad*dcos(alpha/2.d0)+
+c     $       var_rad*(dsin(k*2.d0*alpha/ninner+3.d0*beta/2.d0))
+c          endif
+c        enddo
+c      enddo      
+      
+c      do j = 1,nbodies
+c        do k = 1,ninner
+c        if (j .eq. 1) then
+c          var_rad = 1.d0+0.5d0*dcos(5.d0*k*dtheta)     
+c          x((j-1)*ninner+k) =    
+c     $          0.2d0*var_rad*(dcos(k*dtheta))      
+c          y((j-1)*ninner+k) =      
+c     $          0.2d0*var_rad*(dsin(k*dtheta))
+c        else
+c          var_rad = 0.1d0     
+c          x((j-1)*ninner+k) = 0.25d0+   
+c     $          var_rad*(dcos(k*dtheta))      
+c          y((j-1)*ninner+k) = 0.2d0+     
+c     $          var_rad*(dsin(k*dtheta))         
+c        endif
+c        enddo
+c      enddo            
+c      
       xmin = -8.d-1
       xmax = 8.d-1
       ymin = -0.99d0
@@ -98,12 +161,13 @@ c      enddo
       do j = 1,2*ninner*nbodies + 3*nbodies + 2*nouter
         den(j) = 0.d0
       enddo
-
-      ifmm = 1
-      call stokesSolver(ninner,nbodies,nouter,ifmm,x,y,den,iter)
+      
+      ibary = 1
+      ifmm = 0
+      call stokesSolver(ninner,nbodies,nouter,ifmm,ibary,x,y,den,iter)
 c     pass in number of points and x and y coordinates and return the
 c     density function on the boundary
-
+      print *, 'Solver'
 c      call classifyPoints(ninner,nbodies,x,y,
 c     $      ntargets*ntargets,xtar,ytar,iside,inear)
 c
@@ -113,17 +177,25 @@ c
 c      call computeVelocityPressureTargets(ninner,nbodies,nouter,
 c     $    x,y,den,ntargets*ntargets,xtar,ytar,utar,vtar,press_tar)
 c
-      call computeQoiTargets(ninner,nbodies,nouter,x,y,den,
+
+      
+      if( ibary .eq. 1) then
+      call computeQoiTargetsBaryCen(ninner,nbodies,nouter,x,y,den,
      $  ntargets*ntargets,xtar,ytar,utar,vtar,press_tar,vort_tar)
-
-
-      call computeShearStress(ninner,nbodies,nouter,x,y,den,
+      print *, 'computeQoiTargets_Bary'
+      else 
+      call computeQoiTargetsOld(ninner,nbodies,nouter,x,y,den,
+     $  ntargets*ntargets,xtar,ytar,utar,vtar,press_tar,vort_tar)
+      print *, 'computeQoiTargets_trap'      
+      endif
+      
+      call computeShearStress(ninner,nbodies,nouter,x,y,den,ibary,
      $    shear_stress)
 c     pass in the density function and return the shear_stress
-
+      print *, 'computeShearStress'
       call computePressure(ninner,nbodies,nouter,x,y,den,pressure)
 c     pass in the density function and return the pressure
-
+      print *, 'computePressure'
       call computeDrag(ninner,nbodies,x,y,
      $      shear_stress,pressure,drag)
 c     pass in the shear_stress and pressure and return the drag
@@ -132,14 +204,84 @@ c      write(22,1000) drag(2)
 c      enddo
 c      close(unit=21)
 c      close(unit=22)
-    
+      print *, 'computeDrag' 
+      
+      
+      if ( ibary .eq. 1) then
+        do j = 1,nbodies        
+         do k = 1,ninner
+          theta = dble(k-1)*dtheta
+          var_rad = radius(j) + 0.0001d0
+          xtar_test((j-1)*ninner+k) = centerx(j) + var_rad*
+     $          (dcos(phi(j))*dcos(theta) + 
+     $           2.d0*dsin(phi(j))*dsin(theta))
+          ytar_test((j-1)*ninner+k) = centery(j) + var_rad*
+     $          (-dsin(phi(j))*dcos(theta) + 
+     $           2.d0*dcos(phi(j))*dsin(theta))
+         enddo
+       enddo
+c      do j = 1,nbodies
+c        do k = 1,ninner
+c          beta = pi - alpha
+c          var_rad = radius(j) + 0.0001d0
+c          if(k .lt. ninner/2+1) then
+c          xtar_test((j-1)*ninner+k) = !-var_rad*dcos(pi/4.d0)+
+c     $          var_rad*(dcos(k*2.d0*alpha/ninner+beta/2.d0))
+c          ytar_test((j-1)*ninner+k) = -var_rad*dcos(alpha/2.d0)+
+c     $          var_rad*(dsin(k*2.d0*alpha/ninner+beta/2.d0))
+c          else
+c          xtar_test((j-1)*ninner+k) = !var_rad*dcos(pi/4.d0)+
+c     $       var_rad*(dcos(k*2.d0*alpha/ninner+3.d0*beta/2.d0))
+c          ytar_test((j-1)*ninner+k) = var_rad*dcos(alpha/2.d0)+
+c     $       var_rad*(dsin(k*2.d0*alpha/ninner+3.d0*beta/2.d0))
+c          endif
+c        enddo
+c      enddo
+      
+c      do j = 1,nbodies
+c        do k = 1,ninner 
+c        if(j .eq. 1) then
+c          var_rad = 1.d0+0.5d0*dcos(5.d0*k*dtheta) + 0.0001d0    
+c          xtar_test((j-1)*ninner+k) =    
+c     $          0.2d0*var_rad*(dcos(k*dtheta))      
+c          ytar_test((j-1)*ninner+k) =      
+c     $          0.2d0*var_rad*(dsin(k*dtheta))
+c        else
+c          var_rad = 0.100001d0     
+c          xtar_test((j-1)*ninner+k) = 0.25d0+   
+c     $          var_rad*(dcos(k*dtheta))      
+c          ytar_test((j-1)*ninner+k) = 0.2d0+     
+c     $          var_rad*(dsin(k*dtheta))         
+c        endif     
+c        enddo      
+c      enddo        
+      call computeQoiTargetsBaryCen(ninner,nbodies,nouter,x,y,den,
+     $  ninner*nbodies,xtar_test,ytar_test,utar_test,vtar_test,
+     $  press_tar_test,vort_tar_test) 
+     
+       open(unit=10,file='output/shear_stress_test.dat')     
+       do k = 1,ninner*nbodies
+         write(10,1000) vort_tar_test(k)
+       enddo      
+       close(unit=10)
+      endif
 
+      if ( ibary .eq. 1) then      
       open(unit=1,file='output/den.dat')
       open(unit=2,file='output/shear_stress.dat')
       open(unit=3,file='output/pressure.dat')
       open(unit=4,file='output/drag.dat')
       open(unit=8,file='output/x.dat')
       open(unit=9,file='output/y.dat')
+      else
+      open(unit=1,file='output/trap_den.dat') 
+      open(unit=2,file='output/trap_shear_stress.dat') 
+      open(unit=3,file='output/trap_pressure.dat') 
+      open(unit=4,file='output/trap_drag.dat')      
+      open(unit=8,file='output/trap_x.dat')      
+      open(unit=9,file='output/trap_y.dat')      
+      endif
+      
       do k = 1,2*ninner*nbodies + 2*nouter + 3*nbodies
         write(1,1000) den(k)
       enddo
@@ -159,12 +301,22 @@ c      close(unit=22)
       close(unit=8)
       close(unit=9)
 
+      if ( ibary .eq. 1) then      
       open(unit=1,file='output/xtar.dat')
       open(unit=2,file='output/ytar.dat')
       open(unit=3,file='output/utar.dat')
       open(unit=4,file='output/vtar.dat')
       open(unit=8,file='output/press_tar.dat')
       open(unit=9,file='output/vort_tar.dat')
+      else
+      open(unit=1,file='output/trap_xtar.dat')      
+      open(unit=2,file='output/trap_ytar.dat')      
+      open(unit=3,file='output/trap_utar.dat')      
+      open(unit=4,file='output/trap_vtar.dat')      
+      open(unit=8,file='output/trap_press_tar.dat')      
+      open(unit=9,file='output/trap_vort_tar.dat')      
+      endif
+      
       do k = 1,ntargets**2
         write(1,1000) xtar(k)
         write(2,1000) ytar(k)
