93131a2cbef123b37c2fbd472cb800fcb6e2489f
Nick Moore <mnmoore2@fsu.edu>
2019-04-04 12:16:08 -0400

    NM

--- julia_code/main.jl
+++ julia_code/main.jl
@@ -25,17 +25,20 @@ function erosion(paramsfile::AbstractString, dt::Float64, tfin::Float64)
 end
 # erosion: The main routine to erode a group of bodies.
 function erosion(thlenden::ThLenDenType, params::ParamType)
-	println("Running erosion with dt = ", round(params.dt,sigdigits=3), 
-		" and tfin = ", round(params.tfin,sigdigits=3))
+	println("Running erosion with dt = ", round(params.dt, sigdigits=3), 
+		" and tfin = ", round(params.tfin, sigdigits=3))
 	# Save the output at t=0.
 	nn=0; nfile = 0; tt = 0.;
 	plotnsave(nfile,tt,thlenden,params)
 	# Enter the time loop to apply Runge-Kutta.
 	while(tt < params.tfin - 0.1*params.dt && length(thlenden.thlenvec) > 0)
-		# Advance the variables forward one timestep with RK4.
+		# Print statements
 		nn += 1
 		println("\n\n\nTIME STEP ", nn)
-		println("t/tfin = ", round(tt/params.tfin,sigdigits=3))
+		println("t/tfin = ", round(tt/params.tfin, sigdigits=3))
+		cpumins = round( (time()-params.cput0)/60. , sigdigits=2)
+		println("cpu time = ", cpumins, " minutes.")
+		# Advance the variables forward one timestep with RK4.
 		thlenden, dt = rungekutta2(thlenden, params)
 		tt += dt
 		# Plot and save the data if appropriate.
@@ -47,9 +50,9 @@ function erosion(thlenden::ThLenDenType, params::ParamType)
 	# Plot and save one last time with zero bodies.
 	nfile += 1
 	plotnsave(nfile,tt,thlenden,params)
-	cputime = round( (time()-params.cput0)/60. , sigdigits=3)
+	cputime = round( (time()-params.cput0)/3600. , sigdigits=2)
 	println("\n\n\nCOMPLETED SIMULATION")
-	println("cpu time = ", cputime, " minutes.\n\n")
+	println("cpu time = ", cputime, " hours.\n\n")
 	return thlenden,params,tt,cputime
 end
 
