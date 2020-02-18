e788bc8464e51e5d1a568aa699aa5346af7a6778
Nick Moore <mnmoore2@fsu.edu>
2020-02-18 14:32:18 -0500

    NM

--- julia_code/shrink_circles.jl
+++ julia_code/shrink_circles.jl
@@ -2,19 +2,27 @@
 include("main.jl")
 
 function shrink_circles(paramsfile::AbstractString = "params")
-	thlenden, params = startup(paramsfile)
 	nsteps = 100
-	for nn = 0:nsteps
+	thlenden, params = startup(paramsfile)
+	npts,nbods = getnvals(thlenden.thlenvec)
+	# Initialize the length vector.
+	lenvec0 = zeros(nbods)
+	for  mm = 1:nbods
+		lenvec0[mm] = thlenden.thlenvec[mm].len
+	end
+	# For each step, shrink the circles.
+	for nn = 0:nsteps-1
 		# Scale the lengths by a common factor.
-		lfac = sqrt((nsteps - nn)/nsteps)
-		npts,nbods = getnvals(thlenden.thlenvec)
+		lfac = sqrt((nsteps - nn)/(nsteps))
 		for mm = 1:nbods
-			thlenden.thlenvec[mm].len *= lfac
+			thlenden.thlenvec[mm].len = lfac*lenvec0[mm]
+			thlenden.density = []
+			thlenden.denrot = []
+			thlenden.thlenvec[mm].xx = []
+			thlenden.thlenvec[mm].yy = []
 		end
-		getstress!(thlenden, params)
-		# Plot and save the data.
+		# Plot and save the results.
 		plotnsave(nn,0.,thlenden,params)
 	end
 	postprocess(string("run_",paramsfile))
 end
-
