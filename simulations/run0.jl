38f3c2be3d3193cf8dda765fdc4ed8a5c848aecb
Nick Moore <mnmoore2@fsu.edu>
2021-07-12 16:19:52 -0400

    Added example file that shows how to run an Erosion simulation and post-process the data

---
+++ simulations/run0.jl
@@ -0,0 +1,9 @@
+# OBJECTIVE: An example file that shows how to run an erosion simulation.
+# This file calls the erosion package, initates parameters, 
+# runs the erosion simulation, and then post-processes the data.
+
+using Erosion
+
+params = ParamSet()
+run_erosion(params)
+post_process(outfile(params))
